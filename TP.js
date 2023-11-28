ed19548ddb6e11d8e42b31bc45b8032619a21975
guyssarfanmade <152090545+guyssarfanmade@users.noreply.github.com>
2023-11-28 22:56:12 +0700

    Add files via upload

---
+++ TP.js
@@ -0,0 +1,9 @@
+function ToWSB() {
+    window.location.href = "/wsb.html";
+}
+function ToWS() {
+    window.location.href = "/ws.html";
+}
+function ToWSTem() {
+    window.location.href = "/";
+}
